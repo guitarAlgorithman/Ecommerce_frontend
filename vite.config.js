c31535f7ffdfb45ed8e8f3e684a5fa8fd1555289
sauriokat <gustavo.henriquez.m@gmail.com>
2023-02-17 18:49:37 -0300

    gh 3

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Ecommerce_frontend',
   resolve: {
     alias: {
       web3: 'web3/dist/web3.min.js',
